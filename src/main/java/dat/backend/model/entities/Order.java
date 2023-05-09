4558425bbcd23e0fb726bd593dd29f24945639c6
Andreas Pihl <54024731+bakkary@users.noreply.github.com>
2023-05-09 12:28:52 +0200

    getters and start of setter

--- src/main/java/dat/backend/model/entities/Order.java
+++ src/main/java/dat/backend/model/entities/Order.java
@@ -7,5 +7,65 @@ public class Order {
         private int orderID;
         private String timeStamp;
         private Status status;
-        private ArrayList<AMaterial> materials;
+        private ArrayList<OrderItem> orderItems;
+
+
+        public Order(int orderID, String timeStamp, Status status){
+                this.orderID =orderID;
+                this.timeStamp = timeStamp;
+                this.status = status;
+
+        }
+
+        public void addOrderItem(OrderItem orderItem){
+                orderItem.add(orderItem);
+
+        }
+
+        public void addOrderItems(ArrayList<OrderItem> orderItems){
+                this.orderItems.addAll(orderItems);
+
+        }
+
+
+        public int getOrderID(){
+                return orderID;
+
+        }
+
+        public String getTimeStamp(){
+                return timeStamp;
+
+        }
+
+        public Status getStatus(){
+                return status;
+
+        }
+
+        public ArrayList<OrderItem> getOrderItems(){
+                return orderItems;
+
+        }
+
+        public void setOrderID(int orderID){
+                this.orderID=orderID;
+        }
+
+        public void setTimeStamp()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
