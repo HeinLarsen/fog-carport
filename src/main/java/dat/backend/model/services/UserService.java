d041cd03fb1cd731b65ab28c0f31f9a526f782b7
Tobias Tonndorff <tobias@tonndorff.dk>
2023-05-11 10:11:35 +0200

    no red lines in userService but needs to be evaluated

--- src/main/java/dat/backend/model/services/UserService.java
+++ src/main/java/dat/backend/model/services/UserService.java
@@ -9,18 +9,23 @@ import java.util.ArrayList;
 
 public class UserService {
 
-    public void createUser(User user) {
-        // TODO implement here
+    public void createUser( String first_name, String last_name, String email, String password, String address, int phone_number, int role_id, int membership_id, int zip, ConnectionPool connectionPool) throws DatabaseException {
+        UserFacade.createUser( first_name, last_name, email, password, address, phone_number, role_id, membership_id, zip, connectionPool);
     }
 
-    public void updateUser(User user) {
-        // TODO implement here
+    public void updateUser(int id, String first_name, String last_name, String email, String password, String address, int phone_number, int role_id, int membership_id, int zip, ConnectionPool connectionPool) throws DatabaseException {
+        UserFacade.updateUser(id, first_name, last_name, email, password, address, phone_number, role_id, membership_id, zip,  connectionPool);
+
     }
 
-    public User getUserById(int id) {
-        // TODO implement here
+
+
+    public User getUserById(int id, ConnectionPool connectionPool) throws DatabaseException {
+        UserFacade.getUser(id, connectionPool);
         return null;
     }
+
+
 
     public static ArrayList<User> getAllUsers(ConnectionPool connectionPool) throws DatabaseException {
         UserFacade.getAllUsers(connectionPool);
