86ed43c83afb572616b4921bdab7128f8cfa58ea
XXOlsen <cph-xo@cphbusiness.dk>
2023-05-12 11:30:39 +0200

    Fjernet kommentarene

--- src/main/java/dat/backend/model/services/OrderService.java
+++ src/main/java/dat/backend/model/services/OrderService.java
@@ -13,32 +13,25 @@ public class OrderService {
 
     public static Order getOrderById(int id, ConnectionPool connectionPool) throws DatabaseException {
 
-        //Hente ÉN Order først, hente ved hjælp af OrderFacaden først:
         Order order = OrderFacade.getOrderById(id, connectionPool);
 
-
-        //Hente ordreelementerne ved hjælp af OrderItemFacaden (byID):
         ArrayList<OrderItem> orderItems = OrderItemFacade.getOrderItemsByOrderId(id, connectionPool);
 
-        //Loop igennem orderItems:
         for (OrderItem item : orderItems) {
             item.addMaterial(MaterialFacade.getMaterialById(id, connectionPool));
         }
 
-        //addOrderItems til denne order:
         order.addOrderItems(orderItems);
 
-        //Returnere objektet.
         return order;
+
     }
 
 
-    //Ny metode her, hvor den skal hente ordren med status.
+
     public static ArrayList<Order> getOrderByStatus(Status status, ConnectionPool connectionPool) throws DatabaseException {
         ArrayList<Order> orders = OrderFacade.getOrdersByStatus(status, connectionPool);
 
-
-        //Loop igennem Ordrene og hente ordreelementerne samt materialerne:
         for (Order order : orders) {
             ArrayList<OrderItem> orderItems = OrderItemFacade.getOrderItemsByOrderId(order.getOrderID(), connectionPool);
             for (OrderItem item : orderItems) {
@@ -55,12 +48,9 @@ public class OrderService {
 
     }
 
-    //Her kalder vi metoder fra Facaderne, for at Admin kan Approve en ordre.
     public void updateOrder(Order order, String status, ConnectionPool connectionPool) throws DatabaseException {
-        //Ind I Ordren og kalde status metode, setStatus(status)
         order.setStatus("approved");
 
-        //Kald den fra Orderfacade med ordre objektet.
         OrderFacade.approveOrder(order, connectionPool);
     }
 
