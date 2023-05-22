45af1ea856ba37c68ddce1a4aff7f158e77dfb70
Tobias Tonndorff <tobias@tonndorff.dk>
2023-05-22 09:17:07 +0200

    refactored exceptions from login logic

--- src/main/java/dat/backend/model/persistence/UserFacade.java
+++ src/main/java/dat/backend/model/persistence/UserFacade.java
@@ -32,7 +32,7 @@ public class UserFacade
 
     }
 
-    public static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException, SQLException {
+    public static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException {
         User user = null;
        user = UserMapper.login(email, password, connectionPool);
         return user;
