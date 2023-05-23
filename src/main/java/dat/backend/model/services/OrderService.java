129d6ae41d0e6ba8a7a927b5c370277e6295b39b
Chris_ziegler <60019587+ChriZZZy@users.noreply.github.com>
2023-05-23 09:10:06 +0200

    fix

--- src/main/java/dat/backend/model/services/OrderService.java
+++ src/main/java/dat/backend/model/services/OrderService.java
@@ -57,7 +57,7 @@ public class OrderService {
 
     public static List<OrderItem> generateOrder(Carport carport, ConnectionPool connectionPool) throws DatabaseException {
         List<Wood> woods = MaterialFacade.getAllWood(connectionPool);
-        List<RoofTile> roofTiles = MaterialFacade.getAllRoofTiles(connectionPool);
+        List<Screw> screws = MaterialFacade.getAllScrews(connectionPool);
         List<OrderItem> orderItems = new ArrayList<>();
 
         // Get and put sterns
@@ -72,112 +72,18 @@ public class OrderService {
         // Get and put poles
         orderItems.add(getPoles(carport, woods, OrderItemTask.POLE));
 
-        // Get and put roof tiles
-        orderItems.add(getRoofTiles(carport, roofTiles, OrderItemTask.ROOF_TILE));
+        // get and put screws for the roof
 
-        // Get and put water board
-        orderItems.addAll(getWaterBoard(carport.getLength(), woods, OrderItemTask.WATERBOARD_SIDES));
-        orderItems.addAll(getWaterBoard(carport.getWidth(), woods, OrderItemTask.WATERBOARD_ENDS));
+        orderItems.add(getRoofScrews(carport, screws, OrderItemTask.ROOF_TILE_SCREWS);
+
 
         if (carport.hasShed()) {
             orderItems.add(getShedClothing(carport.getShed(), woods, OrderItemTask.SHED_CLOTHING));
-            orderItems.add(getRims(carport.getLength() - carport.getShed().getLength(), woods, OrderItemTask.RIM));
-            orderItems.add(getRims(carport.getShed().getLength(), woods, OrderItemTask.RIM_SHED));
-            orderItems.addAll(getShedSupports(carport.getShed().getLength(), woods, OrderItemTask.SHED_SUPPORT_POSTS_SIDES));
-            orderItems.addAll(getShedSupports(carport.getWidth(), woods, OrderItemTask.SHED_SUPPORT_POSTS_GABLE));
-            orderItems.add(calculateWoodPlankLengthForZShape(woods, OrderItemTask.SHED_Z));
-        } else {
-            orderItems.add(getRims(carport.getLength(), woods, OrderItemTask.RIM));
         }
+
+
 
         return orderItems;
-    }
-
-    private static OrderItem calculateWoodPlankLengthForZShape(List<Wood> woods, OrderItemTask task) {
-        // Constants for door height and width
-        int height = 210; // cm
-        int width = 50; // cm
-
-        // Calculate the length of the horizontal sections
-        int horizontalLength = width * 2;
-
-        // Calculate the length of the vertical section
-        int verticalLength = height - width;
-
-        // Calculate the total length of the wood plank
-        int totalLength = horizontalLength + verticalLength;
-
-        List<Wood> filteredWoods = filterWoods(woods, wood -> wood.getCategory().equals("lægte") && wood.getLength() >= totalLength);
-
-        // Find the shortest wood plank that can be used
-        Wood wood = Collections.min(filteredWoods, Comparator.comparing(Wood::getLength));
-        OrderItem orderItem = new OrderItem(1, wood.getPrice(), task.getTask());
-        orderItem.setMaterial(wood);
-        return  orderItem;
-
-    }
-
-
-
-    private static List<OrderItem> getShedSupports(int target, List<Wood> woods, OrderItemTask task) {
-        List<Wood> filteredWoods = filterWoods(woods, wood -> wood.getCategory().equals("brædt"));
-        List<Wood> result = findSterns(target, filteredWoods);
-        List<OrderItem> orderItems = new ArrayList<>();
-        Set<Wood> processedWoods = new LinkedHashSet<>();
-
-        for (Wood wood : result) {
-            if (processedWoods.contains(wood)) {
-                continue;
-            }
-
-            int quantity = Collections.frequency(result, wood);
-            double price = wood.getPrice() * quantity;
-            OrderItem orderItem = new OrderItem(quantity, price, task.getTask());
-            orderItem.setMaterial(wood);
-            orderItems.add(orderItem);
-
-            processedWoods.add(wood);
-        }
-
-        return orderItems;
-    }
-
-    private static List<OrderItem> getWaterBoard(int target, List<Wood> woods, OrderItemTask task) {
-        List<Wood> filteredWoods = filterWoods(woods, wood -> wood.isPressureTreated() && wood.getCategory().equals("brædt") && wood.getWidth() == 100);
-        return getShedSupports(target, filteredWoods, task);
-    }
-
-    private static OrderItem getRoofTiles(Carport carport, List<RoofTile> roofTiles, OrderItemTask task) {
-        TreeMap<Integer, RoofTile> roofTileMap = new TreeMap<>();
-        for (RoofTile roofTile : roofTiles) {
-            int tilesRequired = (int) Math.ceil((double) carport.getLength() / roofTile.getWidth());
-            int coverage = tilesRequired * roofTile.getLength() * roofTile.getWidth();
-            roofTileMap.put(coverage, roofTile);
-        }
-        RoofTile bestRoofTile = roofTileMap.firstEntry().getValue();
-        int tilesRequired = (int) Math.ceil((double) carport.getLength() / bestRoofTile.getWidth());
-        double totalPrice = tilesRequired * bestRoofTile.getPrice();
-        OrderItem orderItem = new OrderItem(tilesRequired, totalPrice, task.getTask());
-        orderItem.setMaterial(bestRoofTile);
-        return orderItem;
-    }
-
-    private static OrderItem getRims(int length, List<Wood> woods, OrderItemTask task) {
-        List<Wood> filteredWoods = filterWoods(woods, wood -> wood.getCategory().equals("spærtræ"));
-
-        TreeMap<Integer, Wood> woodMap = new TreeMap<>();
-        for (Wood filteredWood : filteredWoods) {
-            int difference = Math.abs(filteredWood.getLength() - length);
-            woodMap.put(difference, filteredWood);
-        }
-
-        Wood bestWood = woodMap.firstEntry().getValue();
-        int quantity = (bestWood.getLength() / 2 >= length) ? 1 : 2;
-        double price = bestWood.getPrice() * quantity;
-
-        OrderItem orderItem = new OrderItem(quantity, price, task.getTask());
-        orderItem.setMaterial(bestWood);
-        return orderItem;
     }
 
     private static OrderItem getShedClothing(Shed shed, List<Wood> woods, OrderItemTask task) {
@@ -187,11 +93,13 @@ public class OrderService {
 
         int roofOverhang = 30; // 15 cm overlap on each side
         int totalLength = shed.getLength() * 2 + ((shed.getWidth() * 2) - roofOverhang);
-        int amountOfClothing = (int) Math.ceil(totalLength / 7.5);
-        double price = calculatePrice(filteredWoods.get(0), amountOfClothing);
+        double overlap = 7.5; // 3.75 cm overlap on each side
+        int amountOfClothing = (int) Math.ceil(totalLength / overlap);
+        double price = filteredWoods.get(0).getPrice() * amountOfClothing;
 
         OrderItem orderItem = new OrderItem(amountOfClothing, price, task.getTask());
         orderItem.setMaterial(filteredWoods.get(0));
+
         return orderItem;
     }
 
@@ -233,13 +141,15 @@ public class OrderService {
 
         OrderItem orderItem = new OrderItem(poles.size(), price, task.getTask());
         orderItem.setMaterial(poles.get(0));
+
         return orderItem;
     }
 
     private static OrderItem getSpars(int length, int width, List<Wood> woods, OrderItemTask task) {
         List<Wood> filteredWoods = filterWoods(woods, wood -> wood.getCategory().equals("spærtræ"));
 
-        int actualAmountOfSpars = (int) Math.ceil(length / 90.0);
+        double amountOfSpars = length / 90.0;
+        int actualAmountOfSpars = (int) Math.ceil(amountOfSpars);
         List<Wood> spars = new ArrayList<>();
         double price = 0;
 
@@ -255,6 +165,7 @@ public class OrderService {
 
         OrderItem orderItem = new OrderItem(spars.size(), price, task.getTask());
         orderItem.setMaterial(spars.get(0));
+
         return orderItem;
     }
 
@@ -328,18 +239,42 @@ public class OrderService {
         return woodMap.firstEntry().getValue();
     }
 
+    private static List<OrderItem> getRoofScrews(int target, List<Screw> screws, OrderItemTask task) {
+        List<Screw> filteredScrews = filterScrews(screws, screw -> screw.getName().equals("plastmo bundskruer 200 stk"));
+        int amountOfScrews = target * 12;
+        double price = 0;
+        price = filteredScrews.get(0).getPrice();
+
+
+
+
+
+
+
+        OrderItem orderItem = new OrderItem(spars.size(), price, task.getTask());
+        orderItem.setMaterial(spars.get(0));
+
+        return orderItem;
+    }
+
+
+
+    }
+
+    private static List<Screw> filterScrews(List<Screw> screws, Predicate<Screw> predicate) {
+        return screws.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
     private static List<Wood> filterWoods(List<Wood> woods, Predicate<Wood> predicate) {
         return woods.stream()
                 .filter(predicate)
                 .collect(Collectors.toList());
     }
 
-    private static double calculatePrice(Wood wood, int quantity) {
-        return wood.getPrice() * quantity;
-    }
 
-
-    //I dette metode skal vi have regnet carports bredde samt længde ud, beregningsmetode kommer her.
+    //I denne metode skal vi have regnet carports bredde samt længde ud, beregningsmetode kommer her.
     public void addOrder(Order order) {
 
     }
@@ -349,5 +284,7 @@ public class OrderService {
 
         OrderFacade.approveOrder(order, connectionPool);
     }
+
+
 
 }
