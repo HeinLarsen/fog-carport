b64b0495709c791dddcaefaa8e5f76f22358359b
Chris_ziegler <60019587+ChriZZZy@users.noreply.github.com>
2023-05-26 12:36:55 +0200

    made if conditions for OrderItemFacade

--- src/main/java/dat/backend/model/persistence/OrderItemFacade.java
+++ src/main/java/dat/backend/model/persistence/OrderItemFacade.java
@@ -1,10 +1,10 @@
 package dat.backend.model.persistence;
 
-import dat.backend.model.entities.AMaterial;
-import dat.backend.model.entities.OrderItem;
+import dat.backend.model.entities.*;
 import dat.backend.model.exceptions.DatabaseException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class OrderItemFacade {
     public static ArrayList<OrderItem> getOrderItemsByOrderId(int id, ConnectionPool connectionPool) throws DatabaseException {
@@ -15,9 +15,19 @@ public class OrderItemFacade {
         return orderItems;
     }
 
-    public static void createOrderItem(OrderItem orderItem, int orderId, int woodId, ConnectionPool connectionPool) throws DatabaseException {
-        ArrayList<OrderItem> orderItems;
-        OrderItemMapper.createOrderItemWood(orderItem, orderId, woodId, connectionPool);
+    public static void createOrderItem(OrderItem orderItem, int orderId, int itemId, ConnectionPool connectionPool) throws DatabaseException {
+        ArrayList<OrderItem> items = new ArrayList<>();
+        
+        if (orderItem.getMaterial() instanceof Wood) {
+            OrderItemMapper.createOrderItemWood(orderItem, orderId, itemId, connectionPool);
+        } else if (orderItem.getMaterial() instanceof Screw) {
+            OrderItemMapper.createOrderItemScrew(orderItem, orderId, itemId, connectionPool);
+        } else if (orderItem.getMaterial() instanceof Fitting){
+            OrderItemMapper.createOrderItemFitting(orderItem, orderId, itemId, connectionPool);
+        } else if (orderItem.getMaterial() instanceof RoofTile){
+            OrderItemMapper.createOrderItemRoofTile(orderItem, orderId, itemId, connectionPool);
+        }
+
 
     }
 }
